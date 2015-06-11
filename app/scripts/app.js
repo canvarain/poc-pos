028d6da417c416e8b3fc344620238932c5033fbe
Hari Narasimhan <harin76@gmail.com>
2015-06-12 00:34:22 +0530

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pocPosApp
+ * @description
+ * # pocPosApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('pocPosApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch',
+    'ui.router',
+    'angular-storage',
+    'angular-jwt'
+  ]);
